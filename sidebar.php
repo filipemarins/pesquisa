971da580ac9d0040f2be0b6296c164d98cd84013
FelipeMoreira005 <felipeoliver_edc@hotmail.com>
2015-08-25 16:39:44 -0300

    Erros resolvido

--- sidebar.php
+++ sidebar.php
@@ -15,7 +15,6 @@
 		<li><a href="questionario01?parte=08">Habitos Alimentares</a></li>
 		<li><a href="questionario01?parte=09">Doenças Pré-Existentes</a></li>
 		<li><a href="questionario01?parte=10">Odontológico</a></li>
-<<<<<<< HEAD
 		<li>Avalização Corporal</li>
 		<li>Avalização de Stress</li>
 		<li>Avalização de Depressão</li>
@@ -25,7 +24,6 @@
 		<li>Tabagismo</li>
 		<li>Drogas Ilícitas</li>
 		<li><a href="questionario01?parte=19">Antropometria</a></li>
-=======
 		<li>Avaliação Corporal</li>
 		<li><a href="questionario01?parte=12">Avaliação de Stress</a></li>
 		<li><a href="questionario01?parte=13">Avaliação de Depressão</a></li>
@@ -35,8 +33,6 @@
 		<li><a href="questionario01?parte=17">Tabagismo</a></li>
 		<li><a href="questionario01?parte=18">Drogas Ilícitas</a></li>
 		<li><a href="questionario01?parte=19">Antropometria</a></li>
-		
->>>>>>> origin/master
 
 	</ul>
 
