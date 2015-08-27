cd4ce858312013f85315ba64264ac01dd45c1d49
filipeferraz7 <filipeferraz18@gmail.com>
2015-08-27 15:42:46 -0300

    DB - QUESTIONARIO 4

---
+++ questionario01/adicionar/adicionar-parte04.php
@@ -0,0 +1,14 @@
+<?php
+include('../../conecta.php');
+include('../banco/banco-parte03.php');
+require('../logica/logica-usuario.php');
+
+$pai_datahora = date("Y-m-d H:i:s"); 
+
+//----------------------------------------
+
+$soc_rendafamiliar = $_POST['soc_rendafamiliar'];
+$soc_contri = $_POST['soc_contri'];
+$soc_sustentadas = $_POST['soc_sustentadas'];
+$soc_suacontri = $_POST['soc_suacontri'];
+
