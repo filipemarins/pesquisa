c66e3735006dd679f9c1cad3062d4dfbb58703de
filipemarins <lipe_bm@yahoo.com.br>
2015-09-07 18:42:30 -0300

    Correção sidebar

--- sidebar.php
+++ sidebar.php
@@ -15,7 +15,7 @@
 		<li><a href="questionario01?parte=08">Habitos Alimentares</a></li>
 		<li><a href="questionario01?parte=09">Doenças Pré-Existentes</a></li>
 		<li><a href="questionario01?parte=10">Odontológico</a></li>
-		<li>Avalização Corporal</li>
+		<li>Avaliação Corporal</li>
 		<li><a href="questionario01?parte=12">Avaliação de Stress</a></li>
 		<li><a href="questionario01?parte=13">Avaliação de Depressão</a></li>
 		<li><a href="questionario01?parte=14">Ansiedade</a></li>
@@ -34,7 +34,6 @@
 			$pes_id = usuarioCadastrado();
 			$nome =	buscaUsuario($conexao,$pes_id)['pes_nomecompleto'];
 		}
-
 		 ?>
 		 <?php  echo (verifica($nome) ? "Olá <b>" . $nome . "</b><br>": ""); ?>
 
